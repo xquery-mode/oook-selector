e2d5d5cc405197dafe35480c4513d30c06a086e1
Max-Gerd Retzlaff <max-gerd.retzlaff@lambdawerk.com>
2016-09-19 17:04:54 +0200

    add link to slime project and add license and copyright of slime.el

--- xdbc-selector.el
+++ xdbc-selector.el
@@ -1,9 +1,35 @@
 (require 'xdmp-methods)
 (provide 'xdbc-selector)
 
-;; based on slime-selector
+;;;; xdbc selector
 
-;;;; Buffer selector
+;; based on slime-selector, a.k.a. Slime's Buffer selector
+;; see: https://github.com/slime/slime/blob/master/slime.el
+
+;;;; License and Commentary
+
+;;     Copyright (C) 2003  Eric Marsden, Luke Gorrie, Helmut Eller
+;;     Copyright (C) 2004,2005,2006  Luke Gorrie, Helmut Eller
+;;     Copyright (C) 2007,2008,2009  Helmut Eller, Tobias C. Rittweiler
+;;     Copyright (C) 2016  Max-Gerd Retzlaff
+;;
+;;     For a detailed list of contributors, see the manual.
+;;
+;;     This program is free software; you can redistribute it and/or
+;;     modify it under the terms of the GNU General Public License as
+;;     published by the Free Software Foundation; either version 2 of
+;;     the License, or (at your option) any later version.
+;;
+;;     This program is distributed in the hope that it will be useful,
+;;     but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;;     GNU General Public License for more details.
+;;
+;;     You should have received a copy of the GNU General Public
+;;     License along with this program; if not, write to the Free
+;;     Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+;;     MA 02111-1307, USA.
+
 
 (defvar xdbc-selector-methods nil
   "List of buffer-selection methods for the `xdbc-select' command.
