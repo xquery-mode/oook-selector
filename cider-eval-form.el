d6601685220426f92a89fac62739ad63dfc561ce
Max-Gerd Retzlaff <max-gerd.retzlaff@lambdawerk.com>
2016-09-22 14:19:07 +0200

    add cider-eval-form/value

--- cider-eval-form.el
+++ cider-eval-form.el
@@ -1,4 +1,4 @@
-
+;; based on cider-read-and-eval
 (defun cider-eval-form (form &optional namespace)
   "Read a sexp from the minibuffer and output its result to the echo area.
 If NAMESPACE is non-nil, it is sent to cider as current namespace."
@@ -11,5 +11,24 @@ If NAMESPACE is non-nil, it is sent to cider as current namespace."
       (unless (string= "" ns-form)
         (insert ns-form "\n\n"))
       (insert form)
+
       (let ((cider-interactive-eval-override override))
         (cider-interactive-eval form)))))
+
+(defun cider-eval-form/value (form &optional namespace)
+  "Read a sexp from the minibuffer and output its result to the echo area.
+If NAMESPACE is non-nil, it is sent to cider as current namespace."
+  (interactive)
+  (let* ((override cider-interactive-eval-override)
+         (ns-form (format "(ns %s)" (or namespace (cider-current-ns)))))
+    (with-current-buffer (get-buffer-create cider-read-eval-buffer)
+      (erase-buffer)
+      (clojure-mode)
+      (unless (string= "" ns-form)
+        (insert ns-form "\n\n"))
+      (insert form)
+
+      (let ((cider-interactive-eval-override override))
+        (nrepl-dict-get
+         (nrepl-sync-request:eval form (cider-current-connection) (cider-current-session))
+         "value")))))
