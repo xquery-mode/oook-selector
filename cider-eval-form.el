f295d66f1398293de92594bf4babc877e53d2a56
Max-Gerd Retzlaff <max-gerd.retzlaff@lambdawerk.com>
2016-09-22 14:29:31 +0200

    really use the form prepared in the buffer; problem for cider-eval-form/value

--- cider-eval-form.el
+++ cider-eval-form.el
@@ -4,31 +4,35 @@
 If NAMESPACE is non-nil, it is sent to cider as current namespace."
   (interactive)
   (let* ((override cider-interactive-eval-override)
-         (ns-form (format "(ns %s)" (or namespace (cider-current-ns)))))
-    (with-current-buffer (get-buffer-create cider-read-eval-buffer)
-      (erase-buffer)
-      (clojure-mode)
-      (unless (string= "" ns-form)
-        (insert ns-form "\n\n"))
-      (insert form)
+         (ns-form (format "(ns %s)" (or namespace (cider-current-ns))))
+         (new-form (with-current-buffer (get-buffer-create cider-read-eval-buffer)
+                     (erase-buffer)
+                     (clojure-mode)
+                     (unless (string= "" ns-form)
+                       (insert ns-form "\n\n"))
+                     (insert form)
 
-      (let ((cider-interactive-eval-override override))
-        (cider-interactive-eval form)))))
+                     (buffer-string))))
+    (let ((cider-interactive-eval-override override))
+              (cider-interactive-eval new-form))))
 
 (defun cider-eval-form/value (form &optional namespace)
   "Read a sexp from the minibuffer and output its result to the echo area.
 If NAMESPACE is non-nil, it is sent to cider as current namespace."
   (interactive)
   (let* ((override cider-interactive-eval-override)
-         (ns-form (format "(ns %s)" (or namespace (cider-current-ns)))))
-    (with-current-buffer (get-buffer-create cider-read-eval-buffer)
-      (erase-buffer)
-      (clojure-mode)
-      (unless (string= "" ns-form)
-        (insert ns-form "\n\n"))
-      (insert form)
+         (ns-form (format "(ns %s)" (or namespace (cider-current-ns))))
+         (new-form
+          (with-current-buffer (get-buffer-create cider-read-eval-buffer)
+            (erase-buffer)
+            (clojure-mode)
+            (unless (string= "" ns-form)
+              (insert ns-form "\n\n"))
+            (insert form)
 
-      (let ((cider-interactive-eval-override override))
-        (nrepl-dict-get
-         (nrepl-sync-request:eval form (cider-current-connection) (cider-current-session))
-         "value")))))
+            (buffer-string))))
+    (let ((cider-interactive-eval-override override))
+      (nrepl-dict-get
+       (nrepl-sync-request:eval form ;; should be  new-form  but there's a problem with multiple top-level forms
+                                (cider-current-connection) (cider-current-session))
+       "value"))))
