c3db199aef2c3f1361f5c235eba1ded7256a72e7
Max-Gerd Retzlaff <max-gerd.retzlaff@lambdawerk.com>
2016-10-04 11:52:09 +0200

    xdmp-document-load/rest-from-string calls insert-document, pw

--- xdmp-methods.el
+++ xdmp-methods.el
@@ -194,7 +194,7 @@ xdmp:document-load(\"%s\",
     (read-string (format "Directory [%s]: " (or (car xdmp-document-history) "")) nil
                  'xdmp-document-history
                  (car xdmp-document-history))))
-  (let ((form (format "(upload-document-string \"%s\" \"%s%s\" %s)"
+  (let ((form (format "(insert-document \"%s\" \"%s%s\" %s)"
            (replace-regexp-in-string "\"" "\\\\\"" (buffer-string))
            (if (not (string-equal "" directory))
                (file-name-as-directory directory)
