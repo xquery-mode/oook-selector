4282865ee5bfab059f7de058656ee06d5108ed39
Artem Malyshev <proofit404@gmail.com>
2016-10-17 15:23:48 +0300

    Ensure cider is connected.

--- cider-eval-form.el
+++ cider-eval-form.el
@@ -1,12 +1,14 @@
 (require 'cider-any-uruk)
 
 (defun cider-eval-form (form &optional namespace)
+  (cider-ensure-connected)
   (cider-nrepl-request:eval
    form
    (cider-interactive-eval-handler)
    (or namespace (cider-current-ns))))
 
 (defun cider-eval-form/value (form &optional namespace)
+  (cider-ensure-connected)
   (thread-first
       (cider-nrepl-sync-request:eval form namespace)
     (nrepl-dict-get "value")))
