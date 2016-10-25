69a474361bc68a2d7cab356f2863bfabea1e18a2
Max-Gerd Retzlaff <max-gerd.retzlaff@lambdawerk.com>
2016-10-25 12:01:51 +0200

    improve "Unzip gzipped binaries on cider-any-uruk-eval-form": new behavior:  - if binary is zipped, unzip it and just output the contents  - on other binaries: show informational string "<ginary node of <n> bytes {0x<8 bytes>...}"  - if neither is true, just try to convert via Clojure's "str" function (old behaviour)

--- uruk-unzip-binaries.el
+++ uruk-unzip-binaries.el
@@ -1,15 +1,18 @@
 ;;; Unzip gzipped binaries on cider-any-uruk-eval-form
 
 ;;; Commentary:
+;;; - if binary is zipped, unzip it and just output the contents
+;;; - on other binaries: show informational string "<ginary node of <n> bytes {0x<8 bytes>...}"
+;;; - if neither is true, just try to convert via Clojure's "str" function (old behaviour)
 
 ;;; Code:
 
 (require 'cider-eval-form)
 
 (cider-eval-form "
-(defn gunzip
+(defn unzip
   ([string]
-   (gunzip string \"UTF-8\"))
+   (unzip string \"UTF-8\"))
   ([string encoding]
    (let [input-stream (clojure.java.io/input-stream string)]
      (with-open [out (java.io.ByteArrayOutputStream.)]
@@ -17,17 +20,24 @@
        (.close input-stream)
        (.toString out encoding)))))
 
-(defn maybe-unzip [data]
-  (or (and (= (type data) (Class/forName \"[B\"))
-           ;; is byte-array => assume gzipped data and try to unzip
-           (try
-            (gunzip data)
-            ;; if not successfull => fail and let the default case handle it
-            (catch Exception e nil)))
-      ;; something else (string, number, ...) => convert to String using str
-      (str data)))
-  ")
+(defn hexify \"Convert byte sequence to hex string\" [coll]
+  (let [hex [\\0 \\1 \\2 \\3 \\4 \\5 \\6 \\7 \\8 \\9 \\a \\b \\c \\d \\e \\f]]
+      (letfn [(hexify-byte [b]
+        (let [v (bit-and b 0xFF)]
+          [(hex (bit-shift-right v 4)) (hex (bit-and v 0x0F))]))]
+        (apply str (mapcat hexify-byte coll)))))
 
+(defn maybe-unzip [data]
+  (if (= (type data) (Class/forName \"[B\"))
+    ;; is byte-array => assume gzipped data and try to unzip
+    (try
+      (unzip data)
+      ;; if not successfull => returnt info string
+      (catch Exception e 
+        (format \"<binary node of %s bytes> {0x%s%s}\" (alength data) (hexify (take 8 data)) (if (> (alength data) 8) \"...\" \"\"))))
+    ;; something else (string, number, ...) => convert to String using str
+    (str data)))
+  ")
 
 (defun cider-any-uruk-eval-form ()
   "Clojure form for XQuery document revaluation."
