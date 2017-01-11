ce899d4b2bbfd4044ef4777c87dcb8c56a3e7a97
Max-Gerd Retzlaff <max-gerd.retzlaff@lambdawerk.com>
2017-01-11 08:58:34 +0100

    Return key in oook-list-mode loads file from xdmp-buffer-database

--- oook-list-mode.el
+++ oook-list-mode.el
@@ -1,8 +1,21 @@
+(defvar oook-list-mode-keymap
+  (make-sparse-keymap)
+  "keymap for Oook-List mode")
+
+(let ((map oook-list-mode-keymap))
+  (setq oook-list-mode-keymap (make-sparse-keymap))
+  (define-key map (kbd "C-m") (lambda ()
+                              (interactive)
+                              (let ((old-db (xdmp-get-current-database)))
+                                (when xdmp-buffer-database
+                                  (xdmp-select-database xdmp-buffer-database))
+                                (xdmp-show-this)
+                                (when xdmp-buffer-database
+                                  (xdmp-select-database old-db))))))
+
 (define-minor-mode oook-list-mode
   "Interact with document lists of oook"
   :lighter " Oook-List"
-  :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "C-m") 'xdmp-show-this)
-            map))
+  :keymap oook-list-mode-keymap)
 
 (provide 'oook-list-mode)
